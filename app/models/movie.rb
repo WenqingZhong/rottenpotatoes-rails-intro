73187dfe66628b32e0446ceaa2b701b4ffc50899
Wenqing Zhong <wz2557@columbia.edu>
2021-10-11 03:52:09 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,15 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings
-    ratings :ratings
-    return ratings
-  end 
   
+  def self.all_ratings
+    all_ratings=['G','PG','PG-13','R']
+  end
+
   def self.with_ratings(ratings_list)
+    if ratings_list.length>0
+      Movie.where(rating: ratings_list)
+    else
+      Movie.all
+    end
   end 
   
 end
